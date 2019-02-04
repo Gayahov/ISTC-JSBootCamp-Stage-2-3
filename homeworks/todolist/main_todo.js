dba56c6a99675cd9f162cf7a15a6d7f524ea82f7
Gayahov <gayahov@gmail.com>
2019-02-05 00:43:47 +0400

    :)

--- homeworks/todolist/main_todo.js
+++ homeworks/todolist/main_todo.js
@@ -1,31 +1,70 @@
-
+var toDo = [];
 function newElement() {
+
+
     var li = document.createElement("li");
     li.classList.add("li_style")
-    //create check span
+        //create check span
     var check_span = document.createElement("span")
     check_span.classList.add("check")
     check_span.innerHTML = '<i class="fa fa-check" aria-hidden="true"></i>';
+        // create remove span
     li.append(check_span);
-    //add new li
+    var remove_div= document.createElement("div")
+        remove_div.classList.add("remove")
+        remove_div.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
+        li.append(remove_div);
+        //add new li
     var inputValue = document.getElementById("add_input").value;
-    console.log(inputValue);
+    //console.log(inputValue);
     var text = document.createTextNode(inputValue);
-    console.log(text);
+    toDo.unshift(inputValue)
+    //console.log(toDo);
      li.append(text);
-    console.log(li);
+    //console.log(li);
+    
    if (inputValue === "") {
-    document.querySelector("#add_input").style.border = "1px solid #e8a0a1";
+       document.querySelector("#add_input").style.border = "3px solid #e8a0a1";
+       var error =  document.querySelector("#error")
+       error.innerHTML = "please enter what to do!"
    }else {
        document.getElementById("to_do_list").append(li); 
    }
   
-document.getElementById("add_input").value = "";
-// create remove span
-var remove_span = document.createElement("span")
-    remove_span.classList.add("remove")
-    remove_span.innerHTML = '<i class="fa fa-trash" aria-hidden="true"></i>';
-    li.append(remove_span);
+    document.getElementById("add_input").value = "";
+
+    var dd = document.getElementsByClassName("check")
+
+        // mark as done
+   for (i=0; i<dd.length; i++){
+    document.getElementsByClassName("check")[i].onclick = function(){
+        var x = this.parentElement;
+        console.log(x.classList)
+        if (x.classList == "li_style"){
+                x.classList.add("checked") 
+        } else {
+                x.classList.remove("checked")
+        }
+     }
+
+   }
+        // remove checked list
+    var remove = document.getElementsByClassName("remove");
+    //console.log(remove);
+    var i;
+    var removed = []
+    for (i = 0; i < remove.length; i++) {
+        remove[i].onclick = function() {
+            var div = this.parentElement;
+            div.style.display = "none";
+            removed.unshift("div")
+            //console.log(removed)
+
+        };
+    };
+};
+
+var all = document.getElementsByTagName("li");
+console.log(all)
 
 
-}
