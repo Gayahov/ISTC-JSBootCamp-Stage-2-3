67666b6393dc0c53da2b700cdd7000d3511d1053
Gayahov <gayahov@gmail.com>
2019-02-19 17:11:08 +0400

    not finished

--- homeworks/inheritance/main.js
+++ homeworks/inheritance/main.js
@@ -1,12 +1,12 @@
 function myFunction (){
     var name = document.getElementById("name").value;
-    console.log(name);
+    //console.log(name);
     var surname = document.getElementById("surname").value;
-    console.log(surname);
+    //console.log(surname);
     var company= document.getElementById("company").value;
-    console.log(company);
+    //console.log(company);
     var position= document.getElementById("position").value;
-    console.log(position);
+    //console.log(position);
 var employe = function( name, surname, company,position){
     this.name = name;
     this.surname = surname;
@@ -17,9 +17,30 @@ var employe = function( name, surname, company,position){
 
 var john = new employe(name, surname,company,position);
 console.log(john)
-var para = document.createElement("P");
-var t = document.createTextNode( john);
-para.appendChild(t);
-document.body.appendChild(para);
+// var para = document.createElement("form");
+// var t = document.createTextNode( john);
+// console.log (t)
+// para.appendChild(t);
+// document.body.appendChild(para);
 
 }
+
+function Done () {
+    var tag = document.getElementById("tag").value;
+    //console.log(tag);
+    var color = document.getElementById("color").value;
+    var text = document.getElementById("text").value;
+    var background = document.getElementById("background").value;
+    function Constructor (tag, color, text, background) {
+        this.tag = document.createElement(tag);
+        this.color = this.tag.style.color = color;
+        this.text = this.tag.innerText = text;
+        this.background = this.tag.style.backgroundColor = background;
+        this.append = function () {
+            document.body.appendChild(this.tag);
+        };
+        this.append();
+    };
+    var newEl = new Constructor(tag, color, text, background);
+   
+};
--- testes/main.js
+++ testes/main.js
@@ -16,7 +16,7 @@ Person.prototype.calculateAge  = function() {
 };
 Person.prototype.lastName = 'Smith';
 var john = new Person('John', 1990, 'teacher');
-var jane = new Person('Jane', 1969, 'designer');
+//var jane = new Person('Jane', 1969, 'designer');
 var mark = new Person('Mark', 1948, 'retired');
 john.calculateAge();
 jane.calculateAge();
@@ -25,60 +25,24 @@ console.log(john.lastName);
 console.log(jane.lastName);
 console.log(mark.lastName);
 
-
-
-
-
-var john = {
-    name:"John",
-    yearOfBirth :1990,
-    job: "teacher"
-}
-
-var Person = function(name, yearOfBirth,job){
-    this.name = name;
-    this.yearOfBirth = yearOfBirth;
-    this.job = job;
-    this.calculateAge = function() {
-       console.log(2019- this.yearOfBirth) 
-    }
-  
-
-}
-Person.prototype.calculateAge = function() {
-    console.log (2019-this.yearOfBirth)
-} 
-Person.prototype.lastName = "Smith"
-
-
-var John = new Person ("john", 1990, "teacher")
-var Jane = new Person ("jane", 1986, "designer")
-var mark = new Person('Mark', 1948, 'retired');
-John.calculateAge();
-Jane.calculateAge();
-mark.calculateAge()
-console.log (John.lastName)
-
-/////////////////////////////
 // Lecture: Object.create
-/*
+
 var personProto = {
     calculateAge: function() {
         console.log(2016 - this.yearOfBirth);
     }
 };
-var john = Object.create(personProto);
-john.name = 'John';
-john.yearOfBirth = 1990;
-john.job = 'teacher';
+// var john = Object.create(personProto);
+// john.name = 'John';
+// john.yearOfBirth = 1990;
+// john.job = 'teacher';
 var jane = Object.create(personProto, {
     name: { value: 'Jane' },
     yearOfBirth: { value: 1969 },
     job: { value: 'designer' }
 });
-*/
 
-
+/////////////// tarberutyune?
 
 /////////////////////////////
 // Lecture: Primitives vs objects
