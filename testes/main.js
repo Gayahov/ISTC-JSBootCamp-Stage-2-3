46c0e95d71aafc843eb2f12c84dd1eba501cbeee
Gayahov <gayahov@gmail.com>
2019-02-22 23:20:54 +0400

    //

--- testes/main.js
+++ testes/main.js
@@ -407,7 +407,9 @@ class Person6 {
     constructor (name, yearOfBirth, job) {
         this.name = name;
         this.yearOfBirth = yearOfBirth;
-        this.job = job;
+        this.job = 
+        
+        job;
     }
     
     calculateAge() {
